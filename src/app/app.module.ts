61fc184240b5ca90e6ba601db89ee2683cd4abd8
Mamady Sillah <supurpisces310@msn.com>
2020-10-05 20:16:03 -0400

    added div tags to app component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 import { fromEventPattern } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ServersComponent } from './servers/servers.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
+    ServersComponent,
+    
+   
    
   ],
   imports: [
