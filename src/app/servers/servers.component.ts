d098f67c775313bf88f93804d24543eab553bed0
Mamady Sillah <supurpisces310@msn.com>
2020-10-06 20:30:27 -0400

    changed onUpdateServerName function to capture the value of what the user typed

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = "no server was created"
+  serverCreationStatus = "no server was created";
+  serverName = "";
   constructor() {
 
     setTimeout(() => {
@@ -24,6 +25,7 @@ export class ServersComponent implements OnInit {
     this.serverCreationStatus = 'Server was created';
   }
   onUpdateServerName(event: any) {
-      console.log(event);
-  }
+      // console.log(event);
+      this.serverName = (<HTMLInputElement>event.target).value;
+  };
 }
